6cc21fc16e5c2fd8beee54e1f1d5e6d378f3c975
Mayrla <mayrlagd@hotmail.com>
2020-03-02 10:11:31 -0300

    first

---
+++ first_app/lib/models/item.dart
@@ -0,0 +1,20 @@
+class Item{
+  String title;
+  bool done;
+
+  Item({this.title, this.done});
+
+
+
+  //no site 'json to dart' faz a conversao de json para o codigo abaixo
+  Item.fromJson(Map<String, dynamic> json){
+    title = json['title'];
+    done = json['done'];
+  }
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['title'] = this.title;
+    data['done'] = this.done;
+    return data;
+  }
+}
